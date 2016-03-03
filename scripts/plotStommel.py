58b3fcb27f2be113e48faaf72322e831106ea2cf
Joel Kronborg <joelkronborg@hotmail.com>
2016-03-03 09:35:49 +0000

    Added error plotting

--- scripts/plotStommel.py
+++ scripts/plotStommel.py
@@ -9,7 +9,7 @@ import matplotlib.animation as animation
 import math
 
 def ground_truth(lon,lat):
-    #lon, lat = np.meshgrid(lon, lat)
+    #May not be exactly fitted to the curve because grids may be different
     day = 11.6
     r = 1 / (day * 86400)
     beta = 4e-9
@@ -27,7 +27,9 @@ def particleplotting(filenames,tracerfile, mode):
     lon = np.empty((np.size(pfile.variables['lon'],0),np.size(pfile.variables['lon'],1),len(filenames)))
     lat = np.empty((np.size(pfile.variables['lat'],0),np.size(pfile.variables['lat'],1),len(filenames)))
     z = np.empty((np.size(pfile.variables['z'],0),np.size(pfile.variables['z'],1),len(filenames)))
-    fig, ax = plt.subplots()
+    time = pfile.variables['time'][:,:]
+    fig = plt.figure()
+    gs = mpl.gridspec.GridSpec(3, 2)
     labels = ('Analytical', 'RK4', 'EE')    #If you call them in this order. Could add option to set labels
     for i in range(len(filenames)):
         filename = filenames[i]
@@ -35,8 +37,8 @@ def particleplotting(filenames,tracerfile, mode):
         lon[:,:,i] = pfile.variables['lon'][:,:]
         lat[:,:,i] = pfile.variables['lat'][:,:]
         z[:,:,i] = pfile.variables['z'][:,:]
-        x_grid = np.linspace(0, 60, 100)
-        y_grid = np.linspace(0, 60, 100)
+        x_grid = np.linspace(0, 60, 1000)
+        y_grid = np.linspace(0, 60, 1000)
         x_grid, y_grid = np.meshgrid(x_grid, y_grid)
         psi_grid = ground_truth(x_grid, y_grid)
 #        lon = pfile.variables['lon']
@@ -58,9 +60,10 @@ def particleplotting(filenames,tracerfile, mode):
 #            ax.set_ylabel('Latitude')
 #            ax.set_zlabel('Depth')
         if mode == '2d':
+            ax1 = fig.add_subplot(gs[:,0])
             levels = ground_truth(lon[:,0,i], lat[:,0,i])
-            plt.contour(x_grid, y_grid, psi_grid, levels[::-1], colors='0.4', linestyles='dashed')
-            plt.plot(np.transpose(lon[:,:,i]),np.transpose(lat[:,:,i]),'-')
+            ax1.contour(x_grid, y_grid, psi_grid, levels[:], colors='0.4', linestyles='dashed')
+            ax1.plot(np.transpose(lon[:,:,i]),np.transpose(lat[:,:,i]),'.-')
             plt.xlabel('Longitude')
             plt.ylabel('Latitude')
 #        elif mode =='movie2d':
@@ -78,11 +81,20 @@ def particleplotting(filenames,tracerfile, mode):
 #            plt.show()
 
     #Plot error
-    fig2, ax2 = plt.subplots()
+    #fig2, ax2 = plt.subplots()
+    ax2 = fig.add_subplot(gs[0,1])
     for i in range(len(filenames)):
         psi = ground_truth(lon[:,:,i],lat[:,:,i])
         for j in range(len(psi)):
-            plt.plot(psi[j,:] - psi[j,0])
+            ax2.plot(psi[j,:] - psi[j,0])
+    plt.title("Errors")
+    ax3 = fig.add_subplot(gs[1,1])
+    ax3.plot(np.transpose(np.diff(time)))
+    plt.title("Step size")
+    ax4 = fig.add_subplot(gs[2,1])
+#    print np.shape(np.transpose(np.squeeze(lon)))
+    ax4.plot(np.transpose(np.squeeze(lon[:,:])))
+    plt.title("Longitude")
     plt.show()
 
 if __name__ == "__main__":
