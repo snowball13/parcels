d09ca61f34a8886bc2a24ad29139f9df4503a8f9
Thomas-95 <thomass4695@gmail.com>
2016-07-12 09:14:17 +0100

    Add files via upload
    @erikvansebille @mlange05 I've begun work on a new Python script for PARCELS called test_fragmentation.py, largely based on the existing test_peninsula.py file. The aim is to enable particles to break apart once they meet certain conditions eg. age, attrition by wind. So far, it allocates all the particles in a particle set an age, upon which a conditional statement can be based eg. particles move upwards once particles are a day old.
    
    To add particles to the pset, I've defined the pset as a global variable in the moving_eddys_example() function (such that kernel can find it when pset.execute() is called). Also removed the output file in the pset.execute() argument.
    
    N.B. When running the script from the terminal, do so in ipython (since the jit particle class doesn't currently support changes to the particle set size eg: $ python tests/test_fragmentation.py scipy

---
+++ test_fragmentation.py
@@ -0,0 +1,197 @@
+from parcels import Grid, Particle, JITParticle
+from parcels import AdvectionRK4, AdvectionEE, AdvectionRK45
+from argparse import ArgumentParser
+import numpy as np
+import math
+import pytest
+from datetime import timedelta as delta
+
+method = {'RK4': AdvectionRK4, 'EE': AdvectionEE, 'RK45': AdvectionRK45}
+
+
+###------------------------------------------------------------###
+###----------Taken from the moving_eddies.py file--------------###
+###------------------------------------------------------------###
+
+
+def moving_eddies_grid(xdim=200, ydim=350):
+    """Generate a grid encapsulating the flow field consisting of two
+    moving eddies, one moving westward and the other moving northwestward.
+
+    Note that this is not a proper geophysical flow. Rather, a Gaussian eddy is moved
+    artificially with uniform velocities. Velocities are calculated from geostrophy.
+    """
+    # Set NEMO grid variables
+    depth = np.zeros(1, dtype=np.float32)
+    time = np.arange(0., 25. * 86400., 86400., dtype=np.float64)
+
+    # Coordinates of the test grid (on A-grid in deg)
+    lon = np.linspace(0, 4, xdim, dtype=np.float32)
+    lat = np.linspace(45, 52, ydim, dtype=np.float32)
+
+    # Grid spacing in m
+    def cosd(x):
+        return math.cos(math.radians(float(x)))
+    dx = (lon[1] - lon[0]) * 1852 * 60 * cosd(lat.mean())
+    dy = (lat[1] - lat[0]) * 1852 * 60
+
+    # Define arrays U (zonal), V (meridional), W (vertical) and P (sea
+    # surface height) all on A-grid
+    U = np.zeros((lon.size, lat.size, time.size), dtype=np.float32)
+    V = np.zeros((lon.size, lat.size, time.size), dtype=np.float32)
+    P = np.zeros((lon.size, lat.size, time.size), dtype=np.float32)
+
+    # Some constants
+    corio_0 = 1.e-4  # Coriolis parameter
+    h0 = 1  # Max eddy height
+    sig = 0.5  # Eddy e-folding decay scale (in degrees)
+    g = 10  # Gravitational constant
+    eddyspeed = 0.1  # Translational speed in m/s
+    dX = eddyspeed * 86400 / dx  # Grid cell movement of eddy max each day
+    dY = eddyspeed * 86400 / dy  # Grid cell movement of eddy max each day
+
+    [x, y] = np.mgrid[:lon.size, :lat.size]
+    for t in range(time.size):
+        hymax_1 = lat.size / 7.
+        hxmax_1 = .75 * lon.size - dX * t
+        hymax_2 = 3. * lat.size / 7. + dY * t
+        hxmax_2 = .75 * lon.size - dX * t
+
+        P[:, :, t] = h0 * np.exp(-(x-hxmax_1)**2/(sig*lon.size/4.)**2-(y-hymax_1)**2/(sig*lat.size/7.)**2)
+        P[:, :, t] += h0 * np.exp(-(x-hxmax_2)**2/(sig*lon.size/4.)**2-(y-hymax_2)**2/(sig*lat.size/7.)**2)
+
+        V[:-1, :, t] = -np.diff(P[:, :, t], axis=0) / dx / corio_0 * g
+        V[-1, :, t] = V[-2, :, t]  # Fill in the last column
+
+        U[:, :-1, t] = np.diff(P[:, :, t], axis=1) / dy / corio_0 * g
+        U[:, -1, t] = U[:, -2, t]  # Fill in the last row
+
+    return Grid.from_data(U, lon, lat, V, lon, lat, depth, time, field_data={'P': P})
+
+
+###------------------------------------------------------------###
+###-------- My additions, commented where implemented. --------###
+###------------------------------------------------------------###
+		
+@pytest.mark.parametrize('mode', ['scipy', 'jit'])
+def test_pset_add_explicit(grid, mode, npart=100):
+    lon = np.linspace(0, 1, npart, dtype=np.float32)
+    lat = np.linspace(1, 0, npart, dtype=np.float32)
+
+    ptype = JITParticle.ptype()
+
+    pset = grid.ParticleSet(0, lon=[], lat=[], pclass=ptype[mode])
+    for i in range(npart):
+        particle = ptype[mode](lon=lon[i], lat=lat[i], grid=grid)
+        pset.add(particle)
+    assert(pset.size == 100)
+    assert np.allclose([p.lon for p in pset], lon, rtol=1e-12)
+    assert np.allclose([p.lat for p in pset], lat, rtol=1e-12)
+
+
+def fragmentation(particle, grid, time, dt):#, pset):
+
+	particle.age += dt
+	#if particle.age > 172800./2.:
+		#particle.lat += 0.01
+
+	if particle.age > 172800./2:
+		particle = Particle(lon=3.3, lat=46.1, grid=grid)
+		#global pset
+    		pset.add(particle)
+	
+	
+def moving_eddies_example(grid, npart=1, mode='jit', verbose=False,
+                          method=AdvectionRK4):
+    """Configuration of a particle set that follows two moving eddies
+
+    :arg grid: :class Grid: that defines the flow field
+    :arg npart: Number of particles to intialise"""
+
+    # Determine particle class according to mode
+    ParticleClass = JITParticle if mode == 'jit' else Particle
+    # First, we define a custom Particle class to which we add a
+    # custom variable as in test_peninsula.py, line 88.
+
+    class MyParticle(ParticleClass):
+	# JIT compilation requires a-priori knowledge of the particle
+        # data structure, so we define additional variables here.
+	# This is again following test_peninsula.py
+
+        user_vars = {'age':np.float32}
+
+	def __init__(self, *args, **kwargs):
+		super(MyParticle, self).__init__(*args, **kwargs)
+		self.age = 0.
+
+	def __repr__(self):
+            """Custom print function which overrides the built-in"""
+            return "P(%.4f, %.4f)[age=%.5f]" % (self.lon, self.lat,
+                                                          self.age)
+    
+    # Must define pset as a global variable in order for the kernel to 'find' it.
+    global pset
+    pset = grid.ParticleSet(size=npart, pclass=MyParticle,
+                            start=(3.3, 46.), finish=(3.3, 47.8))
+
+    print 'Initial particle set:', pset
+
+    if verbose:
+        print("Initial particle positions:\n%s" % pset)
+
+    # Execte for 2 days, with 5min timesteps and hourly output
+    endtime = delta(days=2)
+    print("MovingEddies: Advecting %d particles for %s" % (npart, str(endtime)))
+
+    k_frag = pset.Kernel(fragmentation)
+    pset.execute(method + k_frag, endtime=endtime, dt=delta(minutes=5),
+                 interval=delta(hours=1), show_movie=True)
+
+    if verbose:
+        print("Final particle positions:\n%s" % pset)
+    print pset
+
+    return 'Final particle set:', pset
+
+
+###------------------------------------------------------------###
+###------------More stuff I don't need to touch----------------###
+###------------------------------------------------------------###
+
+
+if __name__ == "__main__":
+    p = ArgumentParser(description="""
+Example of particle advection around an idealised peninsula""")
+    p.add_argument('mode', choices=('scipy', 'jit'), nargs='?', default='jit',
+                   help='Execution mode for performing RK4 computation')
+    p.add_argument('-p', '--particles', type=int, default=1,
+                   help='Number of particles to advect')
+    p.add_argument('-v', '--verbose', action='store_true', default=False,
+                   help='Print particle information before and after execution')
+    p.add_argument('--profiling', action='store_true', default=False,
+                   help='Print profiling information after run')
+    p.add_argument('-g', '--grid', type=int, nargs=2, default=None,
+                   help='Generate grid file with given dimensions')
+    p.add_argument('-m', '--method', choices=('RK4', 'EE', 'RK45'), default='RK4',
+                   help='Numerical method used for advection')
+    args = p.parse_args()
+    filename = 'moving_eddies'
+
+    # Generate grid files according to given dimensions
+    if args.grid is not None:
+        grid = moving_eddies_grid(args.grid[0], args.grid[1])
+        grid.write(filename)
+
+    # Open grid files
+    grid = Grid.from_nemo(filename, extra_vars={'P': 'P'})
+
+    if args.profiling:
+        from cProfile import runctx
+        from pstats import Stats
+        runctx("moving_eddies_example(grid, args.particles, mode=args.mode, \
+                              verbose=args.verbose, method=method[args.method])",
+               globals(), locals(), "Profile.prof")
+        Stats("Profile.prof").strip_dirs().sort_stats("time").print_stats(10)
+    else:
+        moving_eddies_example(grid, args.particles, mode=args.mode,
+                              verbose=args.verbose, method=method[args.method])
