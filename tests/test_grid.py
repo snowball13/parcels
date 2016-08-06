0cc0c1af892e5d2640bf38fcc087eca81187fcd2
Michael Lange <michael.lange@imperial.ac.uk>
2016-08-06 08:54:05 +0100

    Grid: Update grid tests to account for new internal default shape

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,3 +1,4 @@
+
 from parcels import Grid
 import numpy as np
 import pytest
@@ -22,8 +23,8 @@ def test_grid_from_data(xdim, ydim):
     grid = Grid.from_data(u, lon, lat, v, lon, lat, depth, time)
     u_t = np.transpose(u).reshape((lat.size, lon.size))
     v_t = np.transpose(v).reshape((lat.size, lon.size))
-    assert len(grid.U.data.shape) == 3  # Will be 4 once we use depth
-    assert len(grid.V.data.shape) == 3
+    assert len(grid.U.data.shape) == 4
+    assert len(grid.V.data.shape) == 4
     assert np.allclose(grid.U.data[0, :], u_t, rtol=1e-12)
     assert np.allclose(grid.V.data[0, :], v_t, rtol=1e-12)
 
@@ -39,7 +40,7 @@ def test_grid_from_nemo(xdim, ydim, tmpdir, filename='test_nemo'):
     grid = Grid.from_nemo(filepath)
     u_t = np.transpose(u).reshape((lat.size, lon.size))
     v_t = np.transpose(v).reshape((lat.size, lon.size))
-    assert len(grid.U.data.shape) == 3  # Will be 4 once we use depth
-    assert len(grid.V.data.shape) == 3
+    assert len(grid.U.data.shape) == 4
+    assert len(grid.V.data.shape) == 4
     assert np.allclose(grid.U.data[0, :], u_t, rtol=1e-12)
     assert np.allclose(grid.V.data[0, :], v_t, rtol=1e-12)
