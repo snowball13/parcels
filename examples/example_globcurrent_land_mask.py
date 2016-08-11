877399832de4ea7f8ced61b22b49cbe797999f5d
Thomas-95 <thomass4695@gmail.com>
2016-08-11 16:50:52 +0100

    Written mask_grid into a test function.
    Something of a bodge - the issue of time independent/depth-less fields #41 means that, on line 28, I'm not able to create land_mask as a 'proper' PARCELS field.

--- examples/example_globcurrent_land_mask.py
+++ examples/example_globcurrent_land_mask.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python
 from netCDF4 import Dataset
+from parcels import Grid, Particle
+from example_globcurrent import set_globcurrent_grid
+from datetime import timedelta as delta
 import numpy as np
-from parcels import Grid
 
 
-def test_globcurrent_land_mask():
+def set_land_mask_grid():
+
     grid = set_globcurrent_grid()
 
     tracerfile = 'examples/GlobCurrent_example_data/20020101000000-GLOBCURRENT-L4-CUReul_hs-ALT_SUM-v02.0-fv01.0.nc'
-
     tfile = Dataset(tracerfile, 'r')     # Load NETCDF4 file.
     X = tfile.variables['lon']
     Y = tfile.variables['lat']
@@ -22,6 +23,47 @@ def test_globcurrent_land_mask():
             if not np.isnan(P[i, j]):
                 land_mask[i, j] = 1
             else:
-                land_mask[i, j] = 0       # 1 represents land, 0 represents ocean
+                land_mask[i, j] = 0      # 1 represents land, 0 represents ocean
 
-    grid.land_mask = land_mask
+    grid.land_mask = land_mask  # NEED TO MAKE A PROPER PARCELS FIELD
+
+    return grid
+
+
+def land_kernel(particle, grid, time, dt):
+
+    if grid.land_mask[particle.lon, particle.lat] == 1:
+        particle.on_land = True
+    else:
+        particle.on_land = False
+
+
+def test_land_mask_grid(grid, npart=2):
+
+    class MyParticle(Particle):
+        user_vars = {'on_land': bool}
+
+        def __init__(self, *args, **kwargs):
+            super(MyParticle, self).__init__(*args, **kwargs)
+
+    x = 24.875
+    y = np.array([-39.125, -31.125])
+    pset = grid.ParticleSet(npart, pclass=MyParticle, start=(x, y[0]), finish=(x, y[1]))
+
+    runtime = delta(days=1)
+    dt = delta(minutes=5)
+    interval = delta(hours=2)
+    k_mask = pset.Kernel(land_kernel)
+
+    pset.execute(k_mask, runtime=runtime, dt=dt, interval=interval,
+                 output_file=pset.ParticleFile(name="maskParticle"),
+                 show_movie = False)
+
+    assert(pset[0].on_land == False)
+    assert(pset[1].on_land == True)
+
+
+if __name__ == "__main__":
+
+    grid = set_land_mask_grid()
+    test_land_mask_grid(grid, npart=2)
